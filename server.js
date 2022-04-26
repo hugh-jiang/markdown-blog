4fe372b642f44f5b767189aacb12e40971568c52
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2022-04-26 19:54:48 -0400

    Update blog page template

--- server.js
+++ server.js
@@ -10,13 +10,27 @@ app.use('/articles', articleRouter);
 app.get('/', (req, res) => {
     const articles = [{
         title: 'Test Article',
-        date: Date.now(),
+        author: 'Hugh Jiang',
+        date: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article',
+        author: 'Hugh Jiang',
+        date: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article',
+        author: 'Hugh Jiang',
+        date: new Date(),
         description: 'Test description'
     }];
 
     res.render('index', {articles: articles});
 });
 
-app.listen(5000);
-
-console.log('Listening on port 5000...');
+// Start server on default port or on port 5000
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Listening on port ${port}...`);
