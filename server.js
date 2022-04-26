3cc1cf8a17f7559e28d2590f22348d9194708c16
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2022-04-26 17:07:57 -0400

    Add articles route

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs');
 
+// All requests to /articles/{...} will be served through the articleRouter middleware (route)
+app.use('/articles', articleRouter);
+
 app.get('/', (req, res) => {
-    res.render('index');
+    const articles = [{
+        title: 'Test Article',
+        date: Date.now(),
+        description: 'Test description'
+    }];
+
+    res.render('index', {articles: articles});
 });
 
 app.listen(5000);
+
+console.log('Listening on port 5000...');
