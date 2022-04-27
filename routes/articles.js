6df0fadf9e0967d4717ed6862fd5f314f24b6afb
hugh-jiang <78945990+hugh-jiang@users.noreply.github.com>
2022-04-26 21:38:52 -0400

    Minor style and bug fixes

--- routes/articles.js
+++ routes/articles.js
@@ -3,8 +3,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('In articles');
+router.get('/new', (req, res) => {
+    res.render('new-article');
+});
+
+router.post('/', (req, res) => {
+
 });
 
 module.exports = router;
